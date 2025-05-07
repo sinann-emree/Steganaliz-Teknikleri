cb585f94857953e9099e21b35fd90cd6f89ab8ca
Sinan Emre Gülşen <sinanemregulsen4048@gmail.com>
2025-05-07 14:53:44 +0300

    Update bpcs_steg.py

--- bpcs_steg.py
+++ bpcs_steg.py
@@ -1,12 +1,9 @@
 import numpy as np
 from PIL import Image
 
-# Mesajı gizleme fonksiyonu
 def embed_message_bpcs(image_path, message, output_path):
-    # Görseli aç
     img = np.array(Image.open(image_path).convert('RGB'))
 
-    # Mesajı ikili (binary) formata çevir
     binary_message = ''.join(format(ord(char), '08b') for char in message)
     
     idx = 0
@@ -14,12 +11,9 @@ def embed_message_bpcs(image_path, message, output_path):
     for i in range(height):
         for j in range(width):
             if idx < len(binary_message):
-                # R, G, B bileşenlerini ayrı ayrı işleyelim
-                for k in range(3):  # R, G, B
-                    # LSB'yi değiştir
+                for k in range(3): 
                     new_value = (img[i, j, k] & ~1) | int(binary_message[idx])
                     
-                    # Piksel değerini 0 ile 255 arasında tutmak için sınırlandırma yap
                     if new_value < 0:
                         new_value = 0
                     elif new_value > 255:
@@ -34,9 +28,7 @@ def embed_message_bpcs(image_path, message, output_path):
         if idx >= len(binary_message):
             break
 
-    # Sonuç görselini kaydet
     result_img = Image.fromarray(img)
     result_img.save(output_path)
 
-# Örnek kullanım
 embed_message_bpcs("ornek.jpeg", "Merhaba BPCS testi", "gizli_mesaj_bpcs.jpg")
